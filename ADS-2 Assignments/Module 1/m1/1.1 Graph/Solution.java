7689e99354bd97bfd06bd99898e3359a2c069623
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-10-29 20:55:46 +0530

    "ads2_m1_a1_1: 5/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 Assignments/Module 1/m1/1.1 Graph/Solution.java
+++ ADS-2 Assignments/Module 1/m1/1.1 Graph/Solution.java
@@ -6,9 +6,53 @@ interface Graph {
 	public Iterable<Integer> adj(int v);
 	public boolean hasEdge(int v, int w);
 }
-// class AdjacencyList implements Graph {
-
-// }
+class AdjacencyList implements Graph {
+	Bag<Integer>[] bags;
+	private int vertexval;
+	private int edgenum;
+	AdjacencyList(int vertex) {
+		this.vertexval = vertex;
+		bags = (Bag<Integer>[])new Bag[vertex];
+		this.edgenum = 0;
+	}
+	public int V() {
+		return this.vertexval;
+	}
+	public int E() {
+		return this.edgenum;
+	}
+	public void addEdge(int v, int w) {
+		if (v == w || hasEdge(v, w)) {
+			return;
+		}
+		bags[v].add(w);
+		edgenum++;
+	}
+	public boolean hasEdge(int v, int w) {
+		for (Integer eachval : bags[v]) {
+			if (eachval == w) {
+				return true;
+			}
+		}
+		return false;
+	}
+	public Iterable<Integer> adj(int v) {
+		Queue<Integer> queue = new Queue<>();
+		for (Integer eachval : bags[v]) {
+			queue.enqueue(eachval);
+		}
+		return queue;
+	}
+	public String toString() {
+		if (edgenum == 0) {
+			System.out.println(vertexval + " vertices, " + edgenum + " edges");
+			System.out.println("No edges");
+			return null;
+		}
+		System.out.println(vertexval + " vertices, " + edgenum + " edges");
+		return null;
+	}
+}
 class AdjacencyMatrix implements Graph {
 	private int edgenum;
 	private int[][] adjmatrix;
@@ -25,7 +69,7 @@ class AdjacencyMatrix implements Graph {
 		return this.edgenum;
 	}
 	public void addEdge(int v, int w) {
-		if(v == w || hasEdge(v, w)) {
+		if (v == w || hasEdge(v, w)) {
 			return;
 		}
 		adjmatrix[v][w] = 1;
@@ -39,11 +83,11 @@ class AdjacencyMatrix implements Graph {
 		return null;
 	}
 	public String toString() {
-		 if(edgenum == 0) {
-		 	System.out.println(vertexval + " vertices, " + edgenum + " edges");
-		 	System.out.println("No edges");
-		 	return null;
-		 }
+		if (edgenum == 0) {
+			System.out.println(vertexval + " vertices, " + edgenum + " edges");
+			System.out.println("No edges");
+			return null;
+		}
 		System.out.println(vertexval + " vertices, " + edgenum + " edges");
 		for (int i = 0; i < vertexval; i++) {
 			String str = "";
@@ -62,9 +106,28 @@ class Solution {
 		int vertexnum = Integer.parseInt(scan.nextLine());
 		int edgenum = Integer.parseInt(scan.nextLine());
 		String[] placenames = scan.nextLine().split(",");
+		SeparateChainingHashST<Integer, String> schsobj = new SeparateChainingHashST<>();
+		int count = 0;
+		for (String eachname : placenames) {
+			schsobj.put(count, eachname);
+			count++;
+		}
 		switch (lineone) {
 		case "List":
-			// AdjacencyList listobj = new AdjacencyList();
+			AdjacencyList listobj = new AdjacencyList(vertexnum);
+			for (int k = 0; k < edgenum; k++) {
+				String[] edges = scan.nextLine().split(" ");
+				listobj.addEdge(Integer.parseInt(edges[0]), Integer.parseInt(edges[1]));
+			}
+			listobj.toString();
+			for (int j = 0; j < vertexnum; j++) {
+				String str = "";
+				str = str + schsobj.get(j) + ": ";
+				for (Integer each : listobj.adj(j)) {
+					str = str + schsobj.get(each) + " ";
+				}
+				System.out.println(str);
+			}
 			break;
 		case "Matrix":
 			AdjacencyMatrix matrixobj = new AdjacencyMatrix(vertexnum);
