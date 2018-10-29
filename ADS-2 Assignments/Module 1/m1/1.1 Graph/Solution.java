4c88b5a94939c7bec2579cd25eb14a5fbcec71f3
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-10-29 19:42:06 +0530

    "ads2_m1_a1_1: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 Assignments/Module 1/m1/1.1 Graph/Solution.java
+++ ADS-2 Assignments/Module 1/m1/1.1 Graph/Solution.java
@@ -1,13 +1,66 @@
+import java.util.Scanner;
 interface Graph {
-    public int V();
-    public int E();
-    public void addEdge(int v, int w);
-    public Iterable<Integer> adj(int v);
-    public boolean hasEdge(int v, int w);
+	public int V();
+	public int E();
+	public void addEdge(int v, int w);
+	public Iterable<Integer> adj(int v);
+	public boolean hasEdge(int v, int w);
+}
+// class AdjacencyList implements Graph {
+
+// }
+class AdjacencyMatrix implements Graph {
+	private int edgenum;
+	private int[][] adjmatrix;
+	private int vertexval;
+	AdjacencyMatrix(int vertex) {
+		this.vertexval = vertex;
+		adjmatrix = new int[vertex][vertex];
+		this.edgenum = 0;
+	}
+	public int V() {
+		return this.vertexval;
+	}
+	public int E() {
+		return this.edgenum;
+	}
+	public void addEdge(int v, int w) {
+		adjmatrix[v][w] = 1;
+		edgenum++;
+	}
+	public boolean hasEdge(int v, int w) {
+		return adjmatrix[v][w] == 1;
+	}
+	public Iterable<Integer> adj(int v) {
+		return null;
+	}
+	public String toString() {
+		String str = "";
+		for (int i = 0; i < vertexval; i++) {
+			for (int j = 0; j < vertexval - 1; j++) {
+				str = str + adjmatrix[i][j] + " ";
+			}
+			str = str + adjmatrix[i][vertexval - 1];
+			System.out.println(str);
+		}
+		return null;
+	}
 }
 class Solution {
 	public static void main(String[] args) {
-		System.out.println("main starts here");
+		Scanner scan = new Scanner(System.in);
+		String lineone = scan.nextLine();
+		int vertexnum = Integer.parseInt(scan.nextLine());
+		int edgenum = Integer.parseInt(scan.nextLine());
+		switch (lineone) {
+		case "List":
+			// AdjacencyList listobj = new AdjacencyList();
+			break;
+		case "Matrix":
+			AdjacencyMatrix matrixobj = new AdjacencyMatrix(vertexnum);
+			matrixobj.toString();
+			break;
+		}
 	}
 }
 
