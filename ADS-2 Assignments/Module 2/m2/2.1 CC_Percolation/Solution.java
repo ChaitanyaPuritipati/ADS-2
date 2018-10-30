a07a7f56d00f04573c2797ab1fd7d9e71e66a510
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-10-30 23:08:57 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 Assignments/Module 2/m2/2.1 CC_Percolation/Solution.java
+++ ADS-2 Assignments/Module 2/m2/2.1 CC_Percolation/Solution.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class AdjacencyList {
+class Graph {
 	/**
 	 * { variable for Bags array }.
 	 */
@@ -17,7 +17,7 @@ class AdjacencyList {
 	 *
 	 * @param      vertex  The vertex
 	 */
-	AdjacencyList(final int vertex) {
+	Graph(final int vertex) {
 		this.vertexval = vertex;
 		bags = (Bag<Integer>[]) new Bag[vertex];
 		for (int l = 0; l < vertex; l++) {
@@ -30,7 +30,7 @@ class AdjacencyList {
 	 * Complexity: O(1)
 	 * @return     { description_of_the_return_value }
 	 */
-	public int numberofVertices() {
+	public int vertices() {
 		return this.vertexval;
 	}
 	/**
@@ -38,7 +38,7 @@ class AdjacencyList {
 	 *Complexity: O(1)
 	 * @return     { description_of_the_return_value }
 	 */
-	public int numberofEdges() {
+	public int e() {
 		return this.edgenum;
 	}
 	/**
@@ -86,22 +86,125 @@ class AdjacencyList {
 		}
 		return queue;
 	}
-	/**
-	 * Returns a string representation of the object.
-	 *Complexity: O(1)
-	 * @return     String representation of the object.
-	 */
-	public String toString() {
-		if (edgenum == 0) {
-			System.out.println(vertexval + " vertices, "
-			                   + edgenum + " edges");
-			System.out.println("No edges");
-			return null;
-		}
-		System.out.println(vertexval + " vertices, "
-		                   + edgenum + " edges");
-		return null;
-	}
+	// /**
+	//  * Returns a string representation of the object.
+	//  *Complexity: O(1)
+	//  * @return     String representation of the object.
+	//  */
+	// public String toString() {
+	// 	if (edgenum == 0) {
+	// 		System.out.println(vertexval + " vertices, "
+	// 		                   + edgenum + " edges");
+	// 		System.out.println("No edges");
+	// 		return null;
+	// 	}
+	// 	System.out.println(vertexval + " vertices, "
+	// 	                   + edgenum + " edges");
+	// 	return null;
+	// }
+}
+class CC {
+	
+    private boolean[] marked;
+    
+    private int[] id;
+    
+    private int[] size;
+    
+    private int count;
+
+    
+    public CC(final Graph g) {
+        marked = new boolean[g.vertices()];
+        id = new int[g.vertices()];
+        size = new int[g.vertices()];
+        for (int v = 0; v < g.vertices(); v++) {
+            if (!marked[v]) {
+                dfs(g, v);
+                count++;
+            }
+        }
+    }
+
+   
+    private void dfs(final Graph g, final int v) {
+        marked[v] = true;
+        id[v] = count;
+        size[count]++;
+        for (int w : g.adj(v)) {
+            if (!marked[w]) {
+                dfs(g, w);
+            }
+        }
+    }
+    
+    public int id(final int v) {
+        return id[v];
+    }
+    
+    public boolean connected(final int v, final int w) {
+        return id(v) == id(w);
+    }
+}
+class Percolation {
+    private int cnt;
+    private int[][] grid;
+    private int size;
+    private Graph cd;
+    private CC cc;
+    Percolation(final int n) {
+        grid = new int[n][n];
+        cnt = 0;
+        cd = new Graph((n * n) + 2);
+        cc = new CC(cd);
+        this.size = n;
+    }
+    void open(final int row, final int col) {
+        grid[row][col] = 1;
+        cnt++;
+        if (row == 0) {
+            cd.addEdge(0, component(row, col));
+        }
+        if (row == size - 1) {
+            cd.addEdge((size * size) + 1, component(row, col));
+        }
+        if (row + 1 < size && grid[row][col] == 1) {
+                cd.addEdge(
+                    component(row + 1, col), component(row, col));
+            }
+        if (row - 1 >= 0 && grid[row - 1][col] == 1) {
+                cd.addEdge(
+                    component(row - 1, col), component(row, col));
+        }
+        if (col - 1 >= 0 && grid[row][col - 1] == 1) {
+                cd.addEdge(component(row, col - 1), component(row, col));
+        }
+        if (col + 1 < size && grid[row][col + 1] == 1) {
+                cd.addEdge(
+                    component(row, col + 1), component(row, col));
+        }
+    }
+   
+    int component(final int i, final int j) {
+        return (i) * size + j;
+    }
+    
+    boolean isOpen(final int row, final int col) {
+        return grid[row][col] == 1;
+    }
+  
+    boolean isFull(final int row, final int col) {
+        return grid[row][col] == 0;
+    }
+    
+    int numberofopensites() {
+        return cnt;
+    }
+    
+    boolean percolates() {
+        cc = new CC(cd);
+        return cc.connected(0, (size * size) + 1);
+    }
 }
 class Solution {
 	Solution() {
@@ -110,13 +213,11 @@ class Solution {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int vertex = Integer.parseInt(scan.nextLine());
-		AdjacencyList adjlstobj = new AdjacencyList(vertex + 1);
-		while(scan.hasNext()) {
+		Percolation percoobj = new Percolation(vertex);
+		while (scan.hasNext()) {
 			String[] edges = scan.nextLine().split(" ");
-			adjlstobj.addEdge(Integer.parseInt(edges[0]), Integer.parseInt(edges[1]));
+			percoobj.open(Integer.parseInt(edges[0]) - 1, Integer.parseInt(edges[1]) - 1);
 		}
-		for(Integer each : adjlstobj.adj(1)) {
-			System.out.println(each);
-		}
+		System.out.println(percoobj.percolates());
 	}
 }
