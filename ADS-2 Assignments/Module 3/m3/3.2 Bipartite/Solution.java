e214b401692735cbb55b0519b1888194fc57345f
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-11-01 01:04:58 +0530

    "ads2_m3_a2_1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2 Assignments/Module 3/m3/3.2 Bipartite/Solution.java
+++ ADS-2 Assignments/Module 3/m3/3.2 Bipartite/Solution.java
@@ -1,30 +1,50 @@
 import java.util.Scanner;
-
+/**
+ * Class for bipartite.
+ */
 class Bipartite {
-
+    /**
+     * { isBipartite }.
+     */
     private boolean isBipartite;
-
+    /**
+     * { color array }.
+     */
     private boolean[] color;
-
+    /**
+     * { marked array }.
+     */
     private boolean[] marked;
-
+    /**
+     * { edgeTo array }.
+     */
     private int[] edgeTo;
-
+    /**
+     * { cycle Stack }.
+     */
     private Stack<Integer> cycle;
-
+    /**
+     * Constructs the object.
+     *
+     * @param      g1    The g 1
+     */
     Bipartite(final Graph g1) {
         isBipartite = true;
         color  = new boolean[g1.vertex2()];
         marked = new boolean[g1.vertex2()];
         edgeTo = new int[g1.vertex2()];
-
         for (int v = 0; v < g1.vertex2(); v++) {
             if (!marked[v]) {
                 dfs(g1, v);
             }
         }
-
     }
+    /**
+     * { dfs function }.
+     *
+     * @param      g1    The g 1
+     * @param      v     { Integer }
+     */
     private void dfs(final Graph g1, final int v) {
         marked[v] = true;
         for (int w : g1.adj(v)) {
@@ -46,14 +66,30 @@ class Bipartite {
             }
         }
     }
+    /**
+     * Determines if bipartite.
+     *
+     * @return     True if bipartite, False otherwise.
+     */
     public boolean isBipartite() {
         return isBipartite;
     }
 }
+/**
+ * Class for solution.
+ */
 class Solution {
+    /**
+     * Constructs the object.
+     */
     Solution() {
         //unused.
     }
+    /**
+     * { Main function }.
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int vertices = Integer.parseInt(scan.nextLine());
