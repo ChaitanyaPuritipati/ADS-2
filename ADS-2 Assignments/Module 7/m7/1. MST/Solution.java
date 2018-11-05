052ecee187760544626dbc27434734d9d493fe3c
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-11-05 22:45:13 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2 Assignments/Module 7/m7/1. MST/Solution.java
+++ ADS-2 Assignments/Module 7/m7/1. MST/Solution.java
@@ -3,28 +3,32 @@ import java.util.Scanner;
  * Solution class.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//unused.
-	}
-	/**
-	 * { Main function }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner scan =  new Scanner(System.in);
-		int vertices = Integer.parseInt(scan.nextLine());
-		int edges = Integer.parseInt(scan.nextLine());
-		EdgeWeightedGraph edgegraphobj = new EdgeWeightedGraph(vertices);
-		for (int i = 0; i < edges; i++) {
-			String[] edgedata = scan.nextLine().split(" ");
-			Edge eachedge = new Edge(Integer.parseInt(edgedata[0]), Integer.parseInt(edgedata[1]), Double.parseDouble(edgedata[2]));
-			edgegraphobj.addEdge(eachedge);
-		}
-		KruskalMST kruobj = new KruskalMST(edgegraphobj);
-		StdOut.printf("%.5f", kruobj.weight());
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused.
+    }
+    /**
+     * { Main function }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan =  new Scanner(System.in);
+        int vertices = Integer.parseInt(scan.nextLine());
+        int edges = Integer.parseInt(scan.nextLine());
+        EdgeWeightedGraph edgegraphobj = new EdgeWeightedGraph(vertices);
+        for (int i = 0; i < edges; i++) {
+            String[] edgedata = scan.nextLine().split(" ");
+            Edge eachedge = new Edge(
+                Integer.parseInt(
+                    edgedata[0]), Integer.parseInt(
+                    edgedata[1]), Double.parseDouble(
+                    edgedata[2]));
+            edgegraphobj.addEdge(eachedge);
+        }
+        KruskalMST kruobj = new KruskalMST(edgegraphobj);
+        StdOut.printf("%.5f", kruobj.weight());
+    }
 }
