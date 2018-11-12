5ce72c653b9e9a070b06f7ba30e6fb01f53b347a
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-11-12 23:45:18 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 Assignments/Module 13/m13/Radix Sorts/Solution.java
+++ ADS-2 Assignments/Module 13/m13/Radix Sorts/Solution.java
@@ -1,17 +1,28 @@
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 class Solution {
-	private Solution() {
-		//unused
-	}
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int stringscount = Integer.parseInt(scan.nextLine());
-		String[] strings = new String[stringscount];
-		for (int i = 0; i < stringscount; i++) {
-			strings[i] = scan.nextLine();
-		}
-		Quick3string way3sortobj = new Quick3string();
-		way3sortobj.sort(strings);
-		System.out.println(way3sortobj.toString(strings));
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused
+    }
+    /**
+     * { Main function }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int stringscount = Integer.parseInt(scan.nextLine());
+        String[] strings = new String[stringscount];
+        for (int i = 0; i < stringscount; i++) {
+            strings[i] = scan.nextLine();
+        }
+        Quick3string way3sortobj = new Quick3string();
+        way3sortobj.sort(strings);
+        System.out.println(way3sortobj.toString(strings));
+    }
 }
