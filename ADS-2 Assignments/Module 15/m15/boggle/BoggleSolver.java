ba53c55988f9156c0d5a33473ea9c826ce7d98c5
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-11-15 20:35:59 +0530

    "ads2_m16_a1_1: 0/30 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 Assignments/Module 15/m15/boggle/BoggleSolver.java
+++ ADS-2 Assignments/Module 15/m15/boggle/BoggleSolver.java
@@ -39,7 +39,7 @@ public class BoggleSolver {
 		}
 		for(int l = -1; l < 1; l++) {
 			for(int k = -1; k < 1; k++) {
-				if(k == 0 && l == 0) {
+				if((k == 0 && l == 0) || row < 0 || col < 0) {
 					continue;
 				}
 				dfs(board, visited, row + l, col + k, str);
