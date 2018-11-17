f6838abfb320510fe860ce0201766a8059155b2f
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-11-17 12:09:38 +0530

    "ads2_m18_a1_1: 3/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 Assignments/Module 18/m18/ADS-2Exam-3/Solution.java
+++ ADS-2 Assignments/Module 18/m18/ADS-2Exam-3/Solution.java
@@ -217,6 +217,7 @@ class T9 {
 		HashMap<Integer, TreeSet<String>> suggestwords = new HashMap<>();
 		String[] topwords = new String[k];
 		for(String word : words) {
+			System.out.println(tstobj.get(word) + "frequencies");
 			pqobj.insert(tstobj.get(word));
 			if(suggestwords.containsKey(tstobj.get(word))) {
 				TreeSet getwords = suggestwords.get(tstobj.get(word));
