5362a6fa9483e60531275dc758abaa06b7b1307d
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-11-17 13:07:37 +0530

    "ads2_m18_a1_1: 4/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2 Assignments/Module 18/m18/ADS-2Exam-3/Solution.java
+++ ADS-2 Assignments/Module 18/m18/ADS-2Exam-3/Solution.java
@@ -9,6 +9,13 @@ final class Solution {
     // Don't modify this method.
 
     /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused.
+    }
+
+    /**
      * { Main method }.
      *
      * @param      args  The arguments
@@ -20,7 +27,9 @@ final class Solution {
         switch (cases) {
         case "loadDictionary":
             // input000.txt and output000.txt
-            BinarySearchST<String, Integer> hash = loadDictionary("/Files/t9.csv");
+            BinarySearchST<String, Integer> hash
+                = loadDictionary(
+                      "/Files/t9.csv");
             while (scan.hasNextLine()) {
                 String key = scan.nextLine();
                 System.out.println(hash.get(key));
@@ -32,7 +41,8 @@ final class Solution {
             T9 t9 = new T9(loadDictionary("/Files/t9.csv"));
             while (scan.hasNextLine()) {
                 String prefix = scan.nextLine();
-                for (String each : t9.getAllWords(prefix)) {
+                for (String each : t9.getAllWords(
+                            prefix)) {
                     System.out.println(each);
                 }
             }
@@ -44,13 +54,15 @@ final class Solution {
             int count = 0;
             while (scan.hasNextLine()) {
                 String t9Signature = scan.nextLine();
-                for (String each : t9.potentialWords(t9Signature)) {
+                for (String each : t9.potentialWords(
+                            t9Signature)) {
                     count++;
                     System.out.println(each);
                 }
             }
             if (count == 0) {
-                System.out.println("No valid words found.");
+                System.out.println(
+                    "No valid words found.");
             }
             break;
 
@@ -63,7 +75,8 @@ final class Solution {
                 String line = scan.nextLine();
                 bag.add(line);
             }
-            for (String each : t9.getSuggestions(bag, k)) {
+            for (String each : t9.getSuggestions(
+                        bag, k)) {
                 System.out.println(each);
             }
 
@@ -97,7 +110,8 @@ final class Solution {
      *
      * @return     { description_of_the_return_value }
      */
-    public static String[] toReadFile(final String file) {
+    public static String[] toReadFile(
+        final String file) {
         In in = new In(file);
         return in.readAllStrings();
     }
@@ -109,8 +123,10 @@ final class Solution {
      *
      * @return     { description_of_the_return_value }
      */
-    public static BinarySearchST<String, Integer> loadDictionary(final String file) {
-        BinarySearchST<String, Integer>  st = new BinarySearchST<String, Integer>();
+    public static BinarySearchST<String, Integer> loadDictionary(
+        final String file) {
+        BinarySearchST<String, Integer>  st
+            = new BinarySearchST<String, Integer>();
         // your code goes here
         String[] lines = toReadFile(file);
         for (String line : lines) {
